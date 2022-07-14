350a0dc9a1574349ef14a6a8b467b36d2351ecac
Sycamore <pierre.chassagne@epfl.ch>
2022-07-14 09:45:10 +0200

    Created utilities package

--- utilities/setup.py
+++ utilities/setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
-from glob import glob
-import os
 
-package_name = 'opti_track'
+package_name = 'utilities'
 
 setup(
     name=package_name,
@@ -12,18 +10,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sycamore',
-    maintainer_email='sycamore@todo.todo',
+    maintainer_email='pierre.chassagne@epfl.ch',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'opti_link = opti_track.OptiTrack_link:main',
+            'identification = utilities.identification:main'
         ],
     },
 )
